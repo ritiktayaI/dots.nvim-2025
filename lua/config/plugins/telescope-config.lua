fc4f95be2a28bd0642e2446fe7397a1274a9711f
ritiktayaI <ritiktayal2000@hotmail.com>
2025-06-10 16:30:14 +0530

    updating keymapping for telescope

--- lua/config/plugins/telescope-config.lua
+++ lua/config/plugins/telescope-config.lua
@@ -25,15 +25,17 @@ return {
 
         -- require('telescope').load_extension('fzf')
 
-        vim.keymap.set("n", "<leader>fh", require('telescope.builtin').help_tags)
-        vim.keymap.set("n", "<leader>fd", require('telescope.builtin').find_files)
-        vim.keymap.set("n", "<leader>en", function()
+        vim.keymap.set("n", "<leader>fh", require('telescope.builtin').help_tags) -- (f)ind (h)elp_tags
+        vim.keymap.set("n", "<leader>ff", require('telescope.builtin').find_files)-- (f)ind all (f)iles
+        vim.keymap.set("n", "<leader>fg", require('telescope.builtin').git_files) -- (f)ind (g)it files
+        vim.keymap.set("n", "<leader>cc", require('telescope.builtin').colorscheme) -- (c)hange (c)olorscheme
+        vim.keymap.set("n", "<leader>enc", function() -- (e)dit (n)eovim (c)onfig
             require('telescope.builtin').find_files {
                 cwd = vim.fn.stdpath("config")
             }
         end)
 
-        vim.keymap.set("n", "<leader>ep", function()
+        vim.keymap.set("n", "<leader>enp", function() -- (e)dit (n)eovim (p)ackages
             require('telescope.builtin').find_files {
                 cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
             }
